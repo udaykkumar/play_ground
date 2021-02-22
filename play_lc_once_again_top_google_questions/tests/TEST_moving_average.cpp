b5a45e8ae8b34886aa791e6dc56e779e9ea5fa77
Uk <k.udaykumar1985@gmail.com>
2021-02-22 21:43:27 +0530

    phew float comparisons are he - double hockey sticks

--- play_lc_once_again_top_google_questions/tests/TEST_moving_average.cpp
+++ play_lc_once_again_top_google_questions/tests/TEST_moving_average.cpp
@@ -9,11 +9,25 @@ BOOST_AUTO_TEST_CASE( Case_1 )
 {
 	lc::moving_average m(3);
 
-	BOOST_CHECK( m.next(1)  == 1.00000 );
-	BOOST_CHECK( m.next(10) == 5.50000 );
-	BOOST_CHECK( m.next(3)  == 4.66667 );
-	BOOST_CHECK( m.next(5)  == 6.00000 );
+	double lhs1 = m.next(1);
+	double lhs2 = m.next(10);
+	double lhs3 = m.next(3);
+	double lhs4 = m.next(5);
 
+	double rhs1 = 1.00000;
+	double rhs2 = 5.50000;
+	double rhs3 = 4.66667;
+	double rhs4 = 6.00000;
+
+
+
+	BOOST_CHECK( (int) lhs1 == (int) rhs1 );
+	BOOST_CHECK( (int) lhs2 == (int) rhs2 );
+	BOOST_CHECK( (int) lhs3 == (int) rhs3 );
+	BOOST_CHECK( (int) lhs4 == (int) rhs4 );
+	
+
+	
 }
 BOOST_AUTO_TEST_SUITE_END()
 
