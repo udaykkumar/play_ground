fdb3ce4ad852d5019ac5b91bc128223d0315053f
Uk <k.udaykumar1985@gmail.com>
2021-02-23 20:48:22 +0530

    Problem 5: 	Partially working but wrong logic

--- play_lc_once_again_top_google_questions/src/merge_intervals.cpp
+++ play_lc_once_again_top_google_questions/src/merge_intervals.cpp
@@ -2,6 +2,41 @@
 
 namespace lc
 {
+	std::vector< std::vector<int> > merge( std::vector< std::vector<int>>& intervals)
+	{
+		std::vector< std::vector<int> > res;
+		int window[256] = {-1, };
+		for ( int i = 0 ; i < 256; ++i )
+			window[i] = -1;
 
+		for ( auto interval : intervals )
+		{
+			int start = interval[0];
+			int end   = interval[1];
+
+			for ( ; start <= end ; ++start )
+				window[start] = 1; 
+		}
+
+		int start = -1;
+		int end   = -1;
+		for ( int i = 0; i < 256 ; ++i )
+		{
+			auto v = window[i];
+			if ( v == 1 and start == -1 )
+			{
+				start = i;
+			}
+			else if ( v == -1 and start > -1 )
+			{
+				end = i-1;
+				std::cout << start << " " << end << "\n";
+				res.push_back({start,end});
+				start = -1;
+				end   = -1;
+			}
+		}
+		return res;
+	}
 }
 
--- play_lc_once_again_top_google_questions/tests/TEST_merge_intervals.cpp
+++ play_lc_once_again_top_google_questions/tests/TEST_merge_intervals.cpp
@@ -7,7 +7,55 @@ BOOST_AUTO_TEST_SUITE( TEST_merge_intervals )
 
 BOOST_AUTO_TEST_CASE( Case_1 )
 {
+	std::vector< std::vector<int> > intervals(
+		{
+			{1,3},
+			{2,6},
+			{8,10}, 
+			{15,18}
+		});
+	
+	std::vector< std::vector<int> > result(
+		{
+			{1,6},
+			{8,10}, 
+			{15,18}
+		});
 
+
+	auto res = lc::merge(intervals);
+
+	for ( int i = 0 ; i < res.size() ; ++ i)
+	{
+		BOOST_CHECK(res[i][0] == result[i][0]);
+		BOOST_CHECK(res[i][1] == result[i][1]);
+	}
 }
+
+
+BOOST_AUTO_TEST_CASE( Case_2 )
+{
+	std::vector< std::vector<int> > intervals(
+		{
+			{1,4},
+			{4,5}
+		});
+	
+	std::vector< std::vector<int> > result(
+		{
+			{1,5}
+		});
+
+
+	auto res = lc::merge(intervals);
+
+	for ( int i = 0 ; i < res.size() ; ++ i)
+	{
+		BOOST_CHECK(res[i][0] == result[i][0]);
+		BOOST_CHECK(res[i][1] == result[i][1]);
+	}
+}
+
+
 BOOST_AUTO_TEST_SUITE_END()
 
