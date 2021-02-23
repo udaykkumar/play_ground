77ec2fb10a80b591019bef1ba57516f276f55e6d
Uday <k-uday.kumar@hpe.com>
2021-02-23 21:54:51 +0530

    Problem 6: 	initial commit

---
+++ play_lc_once_again_top_google_questions/src/random_pick_with_weight.cpp
@@ -0,0 +1,15 @@
+#include <random_pick_with_weight.hpp>
+
+namespace lc
+{
+    Solution::Solution(std::vector<int> &w)
+    {
+
+    }
+
+    int Solution::pickIndex()
+    {
+
+    }
+
+}
---
+++ play_lc_once_again_top_google_questions/tests/TEST_random_pick_with_weight.cpp
@@ -0,0 +1,13 @@
+#include <random_pick_with_weight.hpp>
+#include <boost/test/unit_test.hpp>
+
+using namespace boost::unit_test;
+
+BOOST_AUTO_TEST_SUITE( TEST_random_pick_with_weight )
+
+BOOST_AUTO_TEST_CASE( Case_1 )
+{
+
+}
+BOOST_AUTO_TEST_SUITE_END()
+
