50492c36f3601ee22f5caee2a2b6e24a5abffaa7
Uk <k.udaykumar1985@gmail.com>
2021-02-23 08:35:16 +0530

    Problem 4: 	initial commit

--- play_lc_once_again_top_google_questions/src/employee_importance.cpp
+++ play_lc_once_again_top_google_questions/src/employee_importance.cpp
@@ -2,6 +2,16 @@
 
 namespace lc
 {
-
+	int getImportance(std::vector<Employee *> employees, int id)
+	{
+		int importance = employees[id]->importance;
+		if( employees[id]->subordinates.size() > 0 ) {
+			for( auto sid : employees[id]->subordinates ) {
+				importance += getImportance( employees, sid );
+			}	
+		}
+		
+		return importance;
+	}
 }
 
