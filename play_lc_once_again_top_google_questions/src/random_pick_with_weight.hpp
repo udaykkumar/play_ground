4d2aa5e30b4336945f8e5c063a1dc4c67f960430
Uday <k-uday.kumar@hpe.com>
2021-02-25 21:00:15 +0530

    Problem 7: rand missing

--- play_lc_once_again_top_google_questions/src/random_pick_with_weight.hpp
+++ play_lc_once_again_top_google_questions/src/random_pick_with_weight.hpp
@@ -1,6 +1,6 @@
 #ifndef __random_pick_with_weight_HPP_INCLUDED__
 #define __random_pick_with_weight_HPP_INCLUDED__
-
+#include <cstdlib>
 #include <vector>
 /**
 	https://leetcode.com/problems/random-pick-with-weight/
