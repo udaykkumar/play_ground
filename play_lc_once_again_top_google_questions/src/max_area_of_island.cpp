28f90c8b3c08ee82a70926fc918540293a448df5
Uday <k-uday.kumar@hpe.com>
2021-02-26 09:27:47 +0530

    Problem 8: 	okay a clean no-nonsense logic

--- play_lc_once_again_top_google_questions/src/max_area_of_island.cpp
+++ play_lc_once_again_top_google_questions/src/max_area_of_island.cpp
@@ -2,9 +2,26 @@
 
 namespace lc
 {
+	int dfs( std::vector< std::vector< int > > g, int i, int j)
+	{
+		if( i < 0 or i > g.size() or j < 0 or j > g.size() )
+			return 0;
+
+		if( g[i][j] == 0 )
+			return 0;
+
+		return 1 + dfs(g, i-1, j) + dfs(g, i+1, j) + dfs(g, i, j-1) + dfs( g, i, j+1 );
+	}
+	
 	int maxAreaOfIsland(std::vector<std::vector<int>>& grid)
 	{
-		return 0;
+		int ma = 0;
+		for ( int i = 0 ; i < grid.size() ; ++i )
+			for ( int j = 0 ; j < grid.size() ; ++j )
+				ma = std::max( ma, dfs(grid, i, j))
+		return s;
 	}
+
+	
 }
 
--- play_lc_once_again_top_google_questions/tests/TEST_max_area_of_island.cpp
+++ play_lc_once_again_top_google_questions/tests/TEST_max_area_of_island.cpp
@@ -7,7 +7,18 @@ BOOST_AUTO_TEST_SUITE( TEST_max_area_of_island )
 
 BOOST_AUTO_TEST_CASE( Case_1 )
 {
+    std::vector< std::vector<int> > v = {   
+    	{0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+        {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+        {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}
+    };
 
+    BOOST_CHECK( lc::maxAreaOfIsland(v) == 6);
 }
 BOOST_AUTO_TEST_SUITE_END()
 
