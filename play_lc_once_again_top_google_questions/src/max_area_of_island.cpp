d65515738ec7282eb6eac0bb233dad8d80105406
Uday <k-uday.kumar@hpe.com>
2021-02-26 10:20:30 +0530

    Problem 8: 	Final commit

--- play_lc_once_again_top_google_questions/src/max_area_of_island.cpp
+++ play_lc_once_again_top_google_questions/src/max_area_of_island.cpp
@@ -2,23 +2,14 @@
 
 namespace lc
 {
-	int row = 0;
-	int col = 0;
+	int ROW = 0;
+	int COL = 0;
 
-	bool not_in_limits( int i, int j )
+	int dfs( std::vector< std::vector< int > > &g, int i, int j)
 	{
-		return ( i < 0 || i >= row || j < 0 || j >= col );
-	}
-
-	int dfs( std::vector< std::vector< int > > g, int i, int j)
-	{
-		if(not_in_limits(i,j) or g[i][j] == 0 )
-		{
-			std::cout << "Skip this [" << i << "," << j << "] => " ;
-			std::cout << ( not_in_limits(i,j) ? "Unknown" : g[i][j] ) << "\n";
+		if( i < 0 or i >= ROW or j < 0 or j >= COL or g[i][j] == 0 )
 			return 0;
-		}
-
+		
 		g[i][j] = 0; // Key here is this one 
 
 		return 1 + dfs(g, i-1, j) + dfs(g, i+1, j) + dfs(g, i, j-1) + dfs( g, i, j+1 );
@@ -26,13 +17,14 @@ namespace lc
 	
 	int maxAreaOfIsland(std::vector<std::vector<int>>& grid)
 	{
-		row = grid.size();
-		col = grid[0].size();
-		std::cout << " row " << row << " col " << col << "\n";
+		ROW = grid.size();
+		COL = grid[0].size();
 		int ma = 0;
-		for ( int i = 0 ; i < grid.size() ; ++i ) {
-			for ( int j = 0 ; j < grid.size() ; ++j ) {
-				ma = std::max( ma, dfs(grid, i, j));
+		for ( int i = 0 ; i < ROW ; ++i ) {
+			for ( int j = 0 ; j < COL ; ++j ) {
+				if ( grid[i][j] ) {
+					ma = std::max( ma, dfs(grid, i, j));
+				}
 			}
 		}
 		return ma;
--- play_lc_once_again_top_google_questions/tests/TEST_max_area_of_island.cpp
+++ play_lc_once_again_top_google_questions/tests/TEST_max_area_of_island.cpp
@@ -17,8 +17,9 @@ BOOST_AUTO_TEST_CASE( Case_1 )
         {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}
     };
-    std::cout <<  lc::maxAreaOfIsland(v) << "\n";
-    BOOST_CHECK( lc::maxAreaOfIsland(v) == 6);
+    int ma = lc::maxAreaOfIsland(v);
+    std::cout << ma  << "\n";
+    BOOST_CHECK( ma == 6);
 }
 BOOST_AUTO_TEST_SUITE_END()
 
