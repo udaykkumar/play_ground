eaa009d77e3091285cf7385bdfdd114e6b83e25c
Uday <k-uday.kumar@hpe.com>
2021-02-26 10:06:17 +0530

    Problem 8: 	seriously I should review my code before committing

--- play_lc_once_again_top_google_questions/src/max_area_of_island.cpp
+++ play_lc_once_again_top_google_questions/src/max_area_of_island.cpp
@@ -14,7 +14,7 @@ namespace lc
 	{
 		if(not_in_limits(i,j) or g[i][j] == 0 )
 		{
-			std::cout << "Skip this [" << i << "," << j << "] => " << grid[i][j] << "\n";
+			std::cout << "Skip this [" << i << "," << j << "] => " << g[i][j] << "\n";
 			return 0;
 		}
 
