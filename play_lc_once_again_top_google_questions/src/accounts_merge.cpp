2242f646a0fdcce48f0396f1e9fb6e09675aa2b4
Uday <k-uday.kumar@hpe.com>
2021-02-27 18:51:40 +0530

    Problem 9: 	initial commit

--- play_lc_once_again_top_google_questions/src/accounts_merge.cpp
+++ play_lc_once_again_top_google_questions/src/accounts_merge.cpp
@@ -6,28 +6,19 @@ namespace lc
     std::vector< std::vector<std::string> > accounts_merge(
     		std::vector< std::vector<std::string> > &accounts)
     {
-    	std::unordered_map<std::string, std::vector<int>> emails;
-    	for ( int idx = 0 ; idx < accounts.size() ; ++idx )
-    	{
-    		auto l = accounts[idx];
-    		for ( int i = 1; i < l.size() ; ++i )
-    		{
-    			auto email = l[i];
-    			auto kvp   = emails.find(email);
-    			if ( kvp != emails.end() )
-    			{
-    				emails[ email ].push_back(i);
-    			}
-    			else
-    			{
-    				emails.insert( std::pair< std::string, std::vector<int> >( 
-    					email, std::vector<int>({i})) );
-    			}
-    		}
-    	}
+        typedef  std::unordered_map<std::string, std::string> stringmap;
+    	typedef  std::unordered_map<std::string, std::unordered_map<std::string, std::string>> graph;
 
-
-
+        graph g;
+        for ( auto a : accounts )
+        {
+            auto name = a[0];
+            for ( int i = 0 ; i < a.size(); ++i )
+            {
+                //g.insert( std::pair< std::string, stringmap>( name, std::pair() ));
+            }
+        }
+        
         return accounts;
     }
 }
