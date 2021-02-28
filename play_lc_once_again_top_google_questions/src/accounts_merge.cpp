25d37bfd9c11559a28ab629074040b566852f083
Uk <k.udaykumar1985@gmail.com>
2021-02-28 12:52:16 +0530

    Problem 9: 	code

--- play_lc_once_again_top_google_questions/src/accounts_merge.cpp
+++ play_lc_once_again_top_google_questions/src/accounts_merge.cpp
@@ -1,12 +1,95 @@
 #include <accounts_merge.hpp>
-#include <unordered_map>
+#include <set>
 
 namespace lc
 {
-    std::vector< std::vector<std::string> > accounts_merge(
-    		std::vector< std::vector<std::string> > &accounts)
+    struct node
     {
+
+        std::string name_;
+        std::set<std::string> emails_;
+
+        node( std::vector<std::string> records )
+        {
+            name_ = records[0];
+            for( size_t i = 1 ; i < records.size(); ++i )
+                emails_.insert( records[i] );
+        }
+
+        std::string name()
+        {
+            return name_;
+        }
+        std::set< std::string > &emails()
+        {
+            return emails_;
+        }
+
+        void show()
+        {
+            std::cout << "Name " << this->name() << " Emails ";
+            for( auto e : this->emails() )
+                std::cout << e << " ";
+            std::cout << "\n";
+        }
+
+        bool connected( node &n )
+        {
+            for ( auto e : n.emails() )
+            {
+                if( emails().find(e) != emails().end() )
+                    return true;
+            }
+
+            return false;
+        }
+    };
+
+    std::vector<std::vector<std::string>> accounts_merge(
+                                           std::vector<std::vector<std::string>> &accounts)
+    {
+        std::vector< node > nodes;
+        for( auto r : accounts )
+            nodes.push_back( node(r) );
+
+        std::vector< std::vector<int> > m( nodes.size(), std::vector( nodes.size(), 0));
+        for (int i = 0; i < m.size(); ++i)
+        {
+            for (int j = 0; j < m[i].size(); ++j )
+                std::cout << m[i][j] << " ";
+            std::cout << "\n";
+        }
+
+
+        for (int i = 0; i < m.size(); ++i)
+        {
+            for (int j = 0; j < m[i].size(); ++j )
+            {
+                if ( m[i][j] )
+                {
+                    continue;
+                }
+                else
+                {
+                    if( nodes[i].connected(nodes[j]) )
+                    {
+                        m[i][j] = 1;
+                        m[j][i] = 1;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < m.size(); ++i)
+        {
+            for (int j = 0; j < m[i].size(); ++j )
+                std::cout << m[i][j] << " ";
+            std::cout << "\n";
+        }
+
+
         return accounts;
     }
+
 }
 
--- play_lc_once_again_top_google_questions/tests/TEST_accounts_merge.cpp
+++ play_lc_once_again_top_google_questions/tests/TEST_accounts_merge.cpp
@@ -7,7 +7,15 @@ BOOST_AUTO_TEST_SUITE( TEST_accounts_merge )
 
 BOOST_AUTO_TEST_CASE( Case_1 )
 {
+    std::vector< std::vector<std::string> > accounts =
+    {
+        {"John", "johnsmith@mail.com", "john00@mail.com"},
+        {"John", "johnnybravo@mail.com"},
+        {"John", "johnsmith@mail.com", "john_newyork@mail.com"},
+        {"Mary", "mary@mail.com"}
+    };
 
+    lc::accounts_merge(accounts);
 }
 BOOST_AUTO_TEST_SUITE_END()
 
