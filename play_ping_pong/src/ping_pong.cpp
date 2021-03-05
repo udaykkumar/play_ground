d94c26e9d041b6ab5007d7b88b559c1e16fa3fd7
Uday <k-uday.kumar@hpe.com>
2021-03-05 15:55:47 +0530

    ping_pong: 	something more

--- play_ping_pong/src/ping_pong.cpp
+++ play_ping_pong/src/ping_pong.cpp
@@ -27,24 +27,28 @@ struct player {
 	
 	player(std::string n) :
 		name_(n),
-		score_(0)
+		score_(0),
+		run_(true)
 		{ }
 
 	player(const char *n) :
 		name_(n),
-		score_(0)
+		score_(0),
+		run_(true)
 		{ }
 
 	// Copy constructor
     player(const player &p) {
     	name_  = p.name(); 
-    	score_ = p.score(); 
+    	score_ = p.score();
+    	run_   = p.run();
     }
 
     // move constructor
     player (player && p){
     	name_  = std::move(p.name()); 
-    	score_ = std::move(p.score()); 
+    	score_ = std::move(p.score());
+    	run_   = std::move(p.run());
   	}
 
   	void operator ++()
@@ -52,18 +56,24 @@ struct player {
   		++score_;
   	}
 
-	friend std::ostream& operator<<( std::ostream &os , const player p )
+  	void stop ()
+  	{
+  		run_  = false;
+  	}
+
+  	friend std::ostream& operator<<( std::ostream &os , const player p )
 	{
 		os << " " << p.name() << " " << p.score() << "\n";
 		return os;
 	} 
 
-	std::string name() const { return name_; }
-	size_t 	    score() const{ return score_;}
-
+	const std::string 	name() 	const { return name_ ; }
+	const size_t 	    score() const { return score_; }
+	const bool 			run() 	const { return run_  ; }
 private:
 	std::string name_;
 	size_t 		score_;
+	bool 		run_;
 };
 
 static std::string make_name(int i)
@@ -73,7 +83,7 @@ static std::string make_name(int i)
 
 void play(std::shared_ptr<player> &&p)
 {
-	while ( running )
+	while ( p->run() )
 	{	
 		if( get_the_ball() )
 		{
@@ -82,6 +92,7 @@ void play(std::shared_ptr<player> &&p)
 		}
 		//std::this_thread::sleep_for(std::chrono::milliseconds(rand()%23));
 	}
+	std::cout << p->name()  << " finished \n";
 }
 
 
@@ -106,7 +117,9 @@ int main(int argc, char const *argv[])
 		std::cout << "Wait for " << 120 << " seconds remaining " << 120 - i << "\n";
 		std::this_thread::sleep_for( std::chrono::seconds(1));
 	}
-	running = false;
+	
+	for ( auto p : players )
+		p->stop();
 	
 	for ( auto p : players )
 			std::cout << i << " " << *p;
