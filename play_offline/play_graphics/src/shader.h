388e9f333b794b4e9f636a8b6be659e2920f76cc
Uk <k.udaykumar1985@gmail.com>
2021-04-03 10:12:37 +0530

    play_graphics: 	draw some shapes on mac

---
+++ play_offline/play_graphics/src/shader.h
@@ -0,0 +1,15 @@
+/*
+ * shader.h
+ *
+ *  Created on: 03-Apr-2021
+ *      Author: kk
+ */
+
+#ifndef PLAY_OFFLINE_PLAY_GRAPHICS_SRC_SHADER_H_
+#define PLAY_OFFLINE_PLAY_GRAPHICS_SRC_SHADER_H_
+
+
+GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path);
+
+
+#endif /* PLAY_OFFLINE_PLAY_GRAPHICS_SRC_SHADER_H_ */
