82e321351c26ce8956ea948552f6420ea901b3da
Uday <k-uday.kumar@hpe.com>
2021-04-03 12:22:35 +0530

    Play_graphics
       play_wth metrics

---
+++ play_offline/play_graphics/src/glfw_hello_metrices.cpp
@@ -0,0 +1,156 @@
+/*
+ * glfw_hello_metrices.cpp
+ *
+ *  Created on: Apr 3, 2021
+ *      Author: kk
+ */
+
+
+#include <iostream>
+#include <cstdlib>
+
+#if __APPLE__
+#	include <OpenGL/gl3.h>
+#else
+#	include <GL/glew.h>
+#endif
+
+
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include "shader.h"
+#include "fs_utils.h"
+
+int main()
+{
+	if( !glfwInit() )
+	{
+		std::cerr << "Unable to initialize GLFW\n";
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, 4); // 4x anti aliasing
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL
+
+	// Open a window and create its OpenGL context
+	GLFWwindow *window;
+	window = glfwCreateWindow(1024, 768, "Hello metrices", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cerr << "Failed to open GLFW window. ";
+		std::cerr << "If you have an Intel GPU, they are not 3.3 compatible.";
+		std::cerr << " Try the 2.1 version of the tutorials.\n";
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent (window);
+
+#ifndef __APPLE__
+	// Initialize GLEW
+	glewExperimental = true; // Needed in core profile
+	if (glewInit() )
+	{
+		std::cerr << "Failed to initialize GLEW\n";
+		return -1;
+	}
+#endif
+
+	// Ensure we can capture the escape key being pressed below
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	// Dark blue background
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	// Create and compile our GLSL program from the shaders
+	std::string baseDir = playground::utils::getExecutableDir();
+
+	std::string SimpleTransformVertexshader = baseDir + "/" + "SimpleTransform.vertexshader";
+	std::string SingleColorFragmentshader = baseDir + "/" + "SingleColor.fragmentshader";
+	GLuint programID = LoadShaders( SimpleTransformVertexshader.c_str(), SingleColorFragmentshader.c_str() );
+
+// Get a handle for our "MVP" uniform
+	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+
+	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+	glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+	// Or, for an ortho camera :
+	//glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
+	
+	// Camera matrix
+	glm::mat4 View       = glm::lookAt(
+								glm::vec3(4,3,3), // Camera is at (4,3,3), in World Space
+								glm::vec3(0,0,0), // and looks at the origin
+								glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+						   );
+	// Model matrix : an identity matrix (model will be at the origin)
+	glm::mat4 Model      = glm::mat4(1.0f);
+	// Our ModelViewProjection : multiplication of our 3 matrices
+	glm::mat4 MVP        = Projection * View * Model; // Remember, matrix multiplication is the other way around
+
+	static const GLfloat g_vertex_buffer_data[] = { 
+		-1.0f, -1.0f, 0.0f,
+		 1.0f, -1.0f, 0.0f,
+		 0.0f,  1.0f, 0.0f,
+	};
+
+	GLuint vertexbuffer;
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+	do{
+
+		// Clear the screen
+		glClear( GL_COLOR_BUFFER_BIT );
+
+		// Use our shader
+		glUseProgram(programID);
+
+		// Send our transformation to the currently bound shader, 
+		// in the "MVP" uniform
+		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+
+		// 1rst attribute buffer : vertices
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(
+			0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+		);
+
+		// Draw the triangle !
+		glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
+
+		glDisableVertexAttribArray(0);
+
+		// Swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	} // Check if the ESC key was pressed or the window was closed
+	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+		   glfwWindowShouldClose(window) == 0 );
+
+	// Cleanup VBO and shader
+	glDeleteBuffers(1, &vertexbuffer);
+	glDeleteProgram(programID);
+	glDeleteVertexArrays(1, &VertexArrayID);
+
+	// Close OpenGL window and terminate GLFW
+	glfwTerminate();
+
+	return 0;
+}
