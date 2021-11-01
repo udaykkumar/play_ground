700882a869fd7dcf65476e6091c5d2a57dc582e6
Uk <k.udaykumar1985@gmail.com>
2021-11-01 21:39:24 +0530

    pimpl example

---
+++ play_offline/play_cpp_data_structures_and_algorithms/src/023_Queue_pimpl.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <memory>
+
+namespace ds
+{
+	template < typename T >
+	class Node;
+
+	template < typename T >
+	class queue_pimpl;
+
+	template < typename T >
+	class queue 
+	{
+		private :
+			std::shared_ptr<queue_pimpl<T>> pimpl_;
+
+		public :
+			queue();
+			~queue();
+			bool 	is_empty() 		{ return pimpl_->is_empty(); 	} 
+		    T 		front()    		{ return pimpl_->front();    	} 	 
+		    T 		back()     		{ return pimpl_->back();     	}
+		    void 	enqueue(T val) 	{  pimpl_->enqueue(val); 		}
+		    void 	dequeue() 		{  pimpl_->dequeue(); 			}
+	};
+}
