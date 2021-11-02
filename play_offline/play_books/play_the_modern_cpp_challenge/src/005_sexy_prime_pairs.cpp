d9f29f5aa6522682271a7f6840c60efd511c14ce
Uday <k-uday.kumar@hpe.com>
2021-11-02 11:08:24 +0530

    sexy primes

---
+++ play_offline/play_books/play_the_modern_cpp_challenge/src/005_sexy_prime_pairs.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+typedef std::pair < uint32_t, uint32_t > sexy_pair_t;
+typedef std::vector < sexy_pair_t >      list_sexy_pair_t;
+
+list_sexy_pair_t primes( uint32_t x )
+{
+	list_sexy_pair_t  sexypairs;
+	{
+		auto *seive = new bool[x+1];
+
+		for ( uint32_t i{3}, prev{2}, cur{2}; 
+				i < x ; ++i ) {
+
+			if ( seive[i] ) continue;
+
+			cur = i;
+			if ( cur - prev == 6 ) 
+				sexypairs.emplace_back( cur, prev ) ;
+			prev = cur;
+			
+			for ( uint32_t j = i+i ; j < x ; j += i ) 
+				seive[j] = true;
+		}
+
+		delete seive;
+	}
+	return sexypairs;
+}
+
+
+int main(int argc, char const *argv[])
+{
+	uint32_t limit{1025};
+	for ( auto p : primes(limit) ) 
+	{
+		std::cout << "( " << p.first 
+					<< ", " << p.second << " ), " ;
+	}
+	std::cout << std::endl;
+	
+	return 0;
+}
