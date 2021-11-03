bf5253f67f1b6576f552707228549e5c77b65228
Uday <k-uday.kumar@hpe.com>
2021-11-03 15:23:27 +0530

    radix sort - with bugs :-(

--- play_offline/play_books/play_cpp_data_structures_and_algorithms/src/030_radix_sort.cpp
+++ play_offline/play_books/play_cpp_data_structures_and_algorithms/src/030_radix_sort.cpp
@@ -6,6 +6,19 @@
 namespace ds
 {
 
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	///
+	///
+	///	THERE IS SOME BUG -
+	/// 	Valgrind may be a good option 
+	///
+	/// terminate called after throwing an instance of 'std::out_of_range'
+	///  what():  vector::_M_range_check: __n (which is 18446744073709551610) >= this->size() (which is 10)
+	///
+	///
+	///
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
 	void radix_sort( int array[], int size)
 	{
 		/// if zero size array nothing to do
@@ -19,17 +32,14 @@ namespace ds
 				largest = std::max( largest, array[i] );
 		}
 	
-
+		/// everytime create a vector of 10 queues representing
+		/// for each index from 0 - 9
+		/// Count also be a one for a-z or A-Z or whatever it is
+		std::vector< std::queue<int> * > digarr( 10 , new std::queue<int>() );
+			
 		/// Iterate though the width of largest element
 		for ( int r = 1; (largest / r) > 0 ; r *= 10 )
 		{
-
-			/// everytime create a vector of 10 queues representing
-			/// for each index from 0 - 9
-			/// Count also be a one for a-z or A-Z or whatever it is
-
-			std::vector< std::shared_ptr< std::queue<int> >  > digarr( 10 , std::make_shared< std::queue<int> >() );
-			
 			/// construct the Queues based on the index
 			for ( int i = 0 ; i < size ; ++i )
 				digarr.at(  ( array[i] / r ) % 10  )->emplace( array[i] );
@@ -46,6 +56,9 @@ namespace ds
 
 			/// repeat till we process the largest element width times
 		}
+
+		for ( auto v : in digarr )
+			delete v;
 	}
 }
 
@@ -56,7 +69,7 @@ int main(int argc, char const *argv[])
 	
 	utils::just_a_simple_sort(f);
 	utils::just_a_simple_sort_multiple(f);
-	utils::huge_sort_ascending(f, 1000000);
-	utils::huge_sort_descending(f,1000000);
+	utils::huge_sort_ascending(f, 100000);
+	utils::huge_sort_descending(f,100000);
 	return 0;
 }
