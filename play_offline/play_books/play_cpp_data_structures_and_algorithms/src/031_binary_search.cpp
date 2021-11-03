9baa1b8e5f8423c01dd7ac0abb77e84b113a1611
Uday <k-uday.kumar@hpe.com>
2021-11-03 19:44:56 +0530

    fix binary_search

--- play_offline/play_books/play_cpp_data_structures_and_algorithms/src/031_binary_search.cpp
+++ play_offline/play_books/play_cpp_data_structures_and_algorithms/src/031_binary_search.cpp
@@ -7,16 +7,18 @@ namespace ds
 	{
 		while ( start_i <= end_i )
 		{
-			int mid = ( start_i + end_i ) / 2 ;
+			int mid =  start_i + ((end_i - start_i)/2 ) ;
 
 			if ( val == array[mid] )
 				return val;
 
 			if ( val < array[mid] )
-				end_i = mid;
+				end_i = mid-1;
 			else
 				start_i = mid+1;
 		}
+
+
 		return -1;
 	}
 }
@@ -24,6 +26,7 @@ namespace ds
 int main(int argc, char const *argv[])
 {
 	std::cout << " binary_search " << std::endl;
+	
 	{
 		int size{100} ;
 		int *array{utils::generate_sorted_array( size )};
@@ -50,7 +53,7 @@ int main(int argc, char const *argv[])
 		int *array{utils::generate_sorted_array( size )};
 		std::cout << " binary_search for " << size << " worst case " << std::endl;
 		for ( int i{0}; i < size ; ++i ) {
-			 ds::binary_search( array, 0, size-1, -1 );
+			 ds::binary_search( array, 0, size-1, i );
 		}
 		std::cout << " binary_search for " << size << " done " <<std::endl;
 	
