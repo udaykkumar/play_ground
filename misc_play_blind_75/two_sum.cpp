4d38f0deb1b6af75433331d2ef8e4fb8535be34e
Uday <k-uday.kumar@hpe.com>
2022-02-27 14:34:09 +0530

    two sum

--- misc_play_blind_75/two_sum.cpp
+++ misc_play_blind_75/two_sum.cpp
@@ -31,25 +31,53 @@ Output: [0,1]
 
 
  **/
+
 class Solution {
+
+    std::unordered_map< int, size_t > vtoi_;
+
+    void make_vtoi( const std::vector<int> &nums ) {
+        size_t index = 0;
+        for ( auto n : nums  ) {
+            vtoi_[n] = index;
+            ++index;
+        }
+    }
+
+    bool exists_in_map( const int v ) const {
+        return vtoi_.find(v) != vtoi_.end();
+    }
+
+    size_t index_of( const int v ) {
+        return vtoi_[v];
+    }
+
 public:
     std::vector<int> twoSum(std::vector<int> &nums, int target) {
-        std::unordered_map< int, int > map;
-        int i = 0;
-        for (auto n : nums)
-            map.insert(std::make_pair(n, i++));
+        
+        /// Create a map of value to index
+        make_vtoi(nums);
 
-        i = 0;
-        for ( auto n : nums )
-        {
-            int y = (target - n);
-            auto kv = map.find(y);
-            if (kv != map.end() and  kv->second != i)
-                return {i, kv->second};
-            ++i;
+        /// for each element in list
+        for ( size_t i = 0 ; i < nums.size() ; ++i ) {
+
+            /// we look for target - current element
+            int look_for { target - nums.at(i) };
+
+            /// if exists .. 
+            if ( exists_in_map(look_for) ) {
+
+                auto idx { index_of(look_for) };
+                /// Make sure its not looking at it-self
+                if ( idx != i ) {
+
+                    /// all well means we are done
+                    return { (int)idx , (int)i };
+                }
+            }
         }
 
-        return { -1, -1};
+        return { -1, -1 } ;
     }
 };
 
@@ -59,7 +87,8 @@ int main(int argc, char const *argv[]) {
     Solution s;
     {
         std::vector<int> nums{ 1, 2, 3, 4, 5, 6, 7, 8};
-        auto res = s.twoSum(nums, 9);
+        auto res = s.twoSum(nums, 15);
+        std::cout << "{ " << res[0] << " , " << res[1] << " }\n";
     }
 
     return 0;
