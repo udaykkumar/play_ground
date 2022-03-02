e2243fb9adc95b868e8aad834302e31509956d66
Uday <k-uday.kumar@hpe.com>
2022-03-02 19:01:34 +0530

    026_same-tree

--- misc_play_blind_75/026_same-tree.cpp
+++ misc_play_blind_75/026_same-tree.cpp
@@ -47,13 +47,13 @@ struct TreeNode {
 
 class Solution {
 
-
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-    	if ( (not p) and (not q) ) return true;
-    	if ( (not p) and      q  ) return false;
-    	if (     p   and (not q) ) return false;
-		if ( p->val != q->val )    return false;
+		if ( !p and !q ) return true;
+    	if ( !p and  q ) return false;
+    	if (  p and !q ) return false;
+		if ( p->val != q->val )
+			return false;
 		return isSameTree(p->left, q->left) and isSameTree(p->right, q->right);
     }
 };
