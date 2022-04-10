b592e15ff8b9c05776972da2b7458780e464cd42
Uk <k.udaykumar1985@gmail.com>
2022-04-10 20:36:48 +0530

    BTree - Not Quite there

---
+++ projects_play_btree_not_working/src/btree.hxx
@@ -0,0 +1,29 @@
+#pragma once
+#include <memory>
+
+namespace ds {
+
+template <typename KeyType = int, typename ValueType = int>
+struct BTreeNode;
+
+template <typename KeyType = int, typename ValueType = int>
+class BTree {
+private:
+    int count_;
+    std::shared_ptr<BTreeNode<KeyType, ValueType>> m_root;
+
+public:
+    BTree()
+        : m_root(std::make_shared<BTreeNode<KeyType, ValueType>>())
+    {
+    }
+    ~BTree() { }
+
+    void put(const KeyType& k, const ValueType& v);
+    ValueType&& get(const KeyType& t);
+    bool exists(const KeyType& k);
+    void show();
+};
+} // namespace ds
+
+#include "btree_impl.hxx"
